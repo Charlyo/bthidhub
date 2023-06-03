c85c5d00088508a3a22c7301fe8f9bad8e9177cb
charlyo <cgarriga@protonmail.com>
2023-06-03 15:01:10 +0200

    Revert quickstorm tk

--- quickstorm_tk_message_filter.py
+++ quickstorm_tk_message_filter.py
@@ -1,9 +1,14 @@
 # Copyright (c) 2020 ruundii. All rights reserved.
+import time
 
 class QuickStormTKMessageFilter:
 
     def filter_message_to_host(self, msg):
-        return b'\xa1' + msg
+        if len(msg) == 8:
+            # Num lock bug still present. Light does not turn off
+            return b'\xa1\x01' + msg
+        else:
+            return b'\xa1' + msg
 
     def filter_message_from_host(self, msg):
         return msg[1:]
